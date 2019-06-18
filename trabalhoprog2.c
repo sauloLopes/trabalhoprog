613a1dd02349bfc1b04c6db50e8607df673534ac
sauloLopes <51964641+sauloLopes@users.noreply.github.com>
2019-06-18 12:59:22 -0300

    Add files via upload

---
+++ trabalhoprog2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void leArquivo()
+{
+    FILE *pont_config;
+
+    pont_config = fopen("config.txt", "r");
+
+    
+}
+
+void fimDeJogo (int p1, int p2)
+{
+    if (p1>p2)
+        printf("%s Ganhou!!!", j1);
+    else if (p2>p1)
+        printf("%s Ganhou!!!", j2);
+    else (p1==p2)
+        printf("Empate");
+}
+
+int main(int argc char *argv)
+{
+
+    return 0;
+}
